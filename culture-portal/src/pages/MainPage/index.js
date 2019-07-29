f722c10f6484d1a63b9d2999ca313e5eb0066975
Vlad Tataritsky <tataritskii@bk.ru>
2019-07-29 13:31:45 +0300

    feat:  add AuthorOfTheDayComponent

--- culture-portal/src/pages/MainPage/index.js
+++ culture-portal/src/pages/MainPage/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import AuthorOfTheDayComponent from "../../components/AuthorOfTheDayComponent/AuthorOfTheDayComponent";
 const Home = () => (
     // Портал дескрипшн компонент
     // Архитектор дня компонент
     // Группа разработчиков компонент
     // Верстка примерная - убрать этот комментарий когда прикрутим наш сайт.
     <div>
-        <h1>Main page</h1>
+      <AuthorOfTheDayComponent/>
     </div>
 )
 
