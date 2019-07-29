1ba359ebdda282789174918c7abb105564da7b0c
dmtrby <dmitry.mahliui@gmail.com>
2019-07-29 10:36:39 +0300

    fix: fixed css location for component

--- culture-portal/src/components/HeaderComponent/index.js
+++ culture-portal/src/components/HeaderComponent/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Navigation from '../NavigationComponent';
+import './HeaderComponent.css';
 
 const Header = () => (
     <Navigation />
--- culture-portal/src/index.js
+++ culture-portal/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import './assets/styles/styles1.css';
+import './assets/styles/style.css';
 
 ReactDOM.render((
     <BrowserRouter>
