f662030fd7d9d6febd911cb960dc840f5655885b
unknown <tarasikuss@gmail.com>
2019-07-30 23:10:31 +0300

    feat: add authors json file

---
+++ culture-portal/src/authors.js
@@ -0,0 +1,34 @@
+exports.frontmatter = {
+  authors : [
+    {
+      id: '1',
+      imgPath: 'images/tarasikuss.jpg',
+      name: 'tarasikuss',
+      gitLink: 'https://github.com/tarasikuss',
+    },
+    {
+      id: '2',
+      imgPath: 'images/nura.jpg',
+      name: 'Nura-BY-13',
+      gitLink: 'https://github.com/Nura-BY-13',
+    },
+    {
+      id: '3',
+      imgPath: 'images/pavel.jpg',
+      name: 'pavel-leskavets',
+      gitLink: 'https://github.com/pavel-leskavets',
+    },
+    {
+      id: '4',
+      imgPath: 'images/dmtrby.jpg',
+      name: 'dmtrby',
+      gitLink: 'https://github.com/dmtrby',
+    },
+    {
+      id: '5',
+      imgPath: 'images/kektor.jpg',
+      name: 'Kektor44',
+      gitLink: 'https://github.com/Kektor44',
+    },
+  ],
+}
