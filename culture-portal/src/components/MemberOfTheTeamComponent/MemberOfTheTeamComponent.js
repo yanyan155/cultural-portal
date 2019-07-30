bc016b22c6d97e0616c74da64f9bfb96d2b796ae
pavel <leskovecpavel7@gmail.com>
2019-07-30 21:31:04 +0300

    fix: fix links

--- culture-portal/src/components/MemberOfTheTeamComponent/MemberOfTheTeamComponent.js
+++ culture-portal/src/components/MemberOfTheTeamComponent/MemberOfTheTeamComponent.js
@@ -1,36 +1,51 @@
-import React from 'react';
-import './MemberOfTheTeamComponent.css';
+import React from "react";
+import "./MemberOfTheTeamComponent.css";
 import MemberOfTeam from "../../assets/images/Yury_Ananich.jpg";
-import linkOfDev from '/';
+import linkOfDev from "/";
 
-const MemberOfTheTeamComponent = (props) => {
-
+const MemberOfTheTeamComponent = props => {
   return (
     <div className="member">
       <h3>Коллектив авторов</h3>
       <div className="cub">
         <div className="cubspinner">
-          <div className="size1"><a
-            href={linkOfDev}
-            target="_blank"> <img src={MemberOfTeam} alt="Yury_Ananich"/></a></div>
-          <div className="size2"><a href={linkOfDev}
-                                    target="_blank">
-            <img src={MemberOfTeam} alt="Yury_Ananich"/></a></div>
-          <div className="size3"><a
-            href={linkOfDev}
-            target="_blank"> <img src={MemberOfTeam} alt="Yury_Ananich" /></a></div>
-          <div className="size4"><a href={linkOfDev} target="_blank">
-          <img src={MemberOfTeam} alt="Yury_Ananich"/></a>
+          <div className="size1">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
+          <div className="size2">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
+          <div className="size3">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
+          <div className="size4">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
+          <div className="size5">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              {" "}
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
+          <div className="size6">
+            <a href={linkOfDev} target="_blank" rel="noopener noreferrer">
+              <img src={MemberOfTeam} alt="Yury_Ananich" />
+            </a>
+          </div>
         </div>
-        <div className="size5"><a href={linkOfDev} target="_blank"> <img
-                                  src={MemberOfTeam} alt="Yury_Ananich" /></a></div>
-        <div className="size6"><a href={linkOfDev}
-                                  target="_blank">
-          <img src={MemberOfTeam} alt="Yury_Ananich"/></a></div>
       </div>
     </div>
-</div>
-)
-}
+  );
+};
 
 export default MemberOfTheTeamComponent;
