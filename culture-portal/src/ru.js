1eab3f027c08eb46236a493a4484e5c7731ada31
unknown <tarasikuss@gmail.com>
2019-07-30 21:21:54 +0300

    feat: add ru json file

---
+++ culture-portal/src/ru.js
@@ -0,0 +1,236 @@
+exports.frontmatter = {
+  authors : [
+    {
+      id: '1',
+      path: '/Ananich',
+      name: 'Юрий Степанович Ананич',
+      date: '23 ноября 1955 - 28 февраля 2015',
+      img: 'ananich.jpg',
+      videoId: 'LiSW2jgILeQ',
+      vita:
+        'Белорусский архитектор. Член Белорусского союза архитекторов.',
+      authorOfTheDay: 'true',
+      lng: 'ru',
+      birthPlace: 'Минск',
+      timelineData: [
+        {
+          date: '1978',
+          text:
+            'Закончил архитектурный факультет Белорусского государственного политехнического института (сейчас БНТУ) по специальности «градостроительство».',
+        },
+        {
+          date: '1979-2009',
+          text:
+            'После завершения учёбы на протяжении тридцати лет работал в РУП «Институт Белгоспроект».',
+        },
+        {
+          date: '2009',
+          text:
+            'участвовал в проектировании трех высотных зданий, которые российская компания «Итера» планировала построить в дополнение к жилому небоскребу «Парус» в рамках многофункционального комплекса «Бизнес-центр по ул. М.Танка».',
+        },
+        {
+          date: '2015',
+          text:
+            'Трагически погиб на железнодорожной станции Птичь Минского района',
+        },
+      ],
+      work: [
+        {
+          title: 'Жилой комплекс посольства Российской федерации в Беларуси',
+          width: 53.898,
+          length: 27.56,
+          date: '2007',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Russian_Embassy_1.jpg/330px-Russian_Embassy_1.jpg',
+        },
+        {
+          title:
+            'Головной офис БСБ Банк по улице Заславской в Минске',
+          width: 53.898,
+          length: 27.551,
+          date: '2016',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/BelSwissBank_Headquarters.jpg/300px-BelSwissBank_Headquarters.jpg',
+        },
+        {
+          title: 'Здание филиала № 511 АСБ «Беларусбанк»',
+          date: '2012',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Belarusbank_511.jpg/300px-Belarusbank_511.jpg',
+        },
+        {
+          title: 'Жилой комплекс посольства Российской федерации',
+          width: 53.898331,
+          length: 27.560555,
+          date: '2007',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Russian_Embassy_2.jpg/300px-Russian_Embassy_2.jpg',
+        },
+        {
+          title: 'Реконструкция здания Национального банка Беларуси',
+          date: '2002',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/NBRB.JPG/269px-NBRB.JPG',
+        },
+      ],
+    },
+    {
+      id: '2',
+      path: '/Levin',
+      name: 'Леонид Менделевич Левин',
+      date: '25 июля 1936 - 1 марта 2014',
+      img: 'levin.jpg',
+      videoId: 'qoWJgqu47gA',
+      vita:
+        'Заслуженный архитектор Республики Беларусь.',
+      authorOfTheDay: 'false',
+      lng: 'ru',
+      birthPlace: 'Минск',
+      timelineData: [
+        {
+          date: '1954-1960',
+          text:
+            'Учёба в Белорусском политехническом институте.',
+        },
+        {
+          date: '1960',
+          text:
+            'Работа в Институте «Минскпроект» (от архитектора до руководителя Мастерской по застройке центра Минска).',
+        },
+        {
+          date: '1960-1967',
+          text:
+            'Участие в создании парка им. 50-летия Советской власти.',
+        },
+        {
+          date: '1968',
+          text:
+            'Участие в создании выставочного павильона ВДНХ БССР в Минске.',
+        },
+        {
+          date: '1970',
+          text:
+            'В составе авторского коллектива был удостоен Ленинской премии за мемориальный комплекс 1968—1969 гг. «Хатынь».',
+        },
+      ],
+      work: [
+        {
+          title: 'Мемориальный ансамбль «Хатынь»',
+          width: 53.898,
+          length: 27.56,
+          date: '1969',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Khatyn_Memorial%2C_Belarus.jpg/401px-Khatyn_Memorial%2C_Belarus.jpg',
+        },
+        {
+          title:
+            'Памятник Янке Купале',
+          width: 53.898,
+          length: 27.551,
+          date: '1972',
+          img:
+            'https://upload.wikimedia.org/wikipedia/ru/thumb/f/ff/Yanka-kupala-minsk.jpg/345px-Yanka-kupala-minsk.jpg',
+        },
+        {
+          title:
+            'Памятник Якубу Коласу',
+          width: 53.898,
+          length: 27.551,
+          date: '1972',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Minsk_Thinker_-_panoramio.jpg/401px-The_Minsk_Thinker_-_panoramio.jpg',
+        },
+        {
+          title: 'Станция метро «Площадь Ленина»',
+          width: 53.898331,
+          length: 27.560555,
+          date: '1984',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Lenpl_03.jpg/375px-Lenpl_03.jpg',
+        },
+        {
+          title: '«Троицкое предместье»',
+          width: 53.898331,
+          length: 27.560555,
+          date: '1980—1986',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Traeckaje_suburb-3.jpg/375px-Traeckaje_suburb-3.jpg',
+        },
+      ],
+    },
+    {
+      id: '3',
+      path: '/Dzuzeppe',
+      name: 'Джузеппе де Сакко',
+      date: '1735 — 1798',
+      img: 'dzuzeppe.jpg',
+      videoId: 'TkaLjjNj8Ls',
+      vita:
+        'Один из крупнейших архитекторов развитого барокко и классицизма, работавших на территории современной Беларуси',
+      authorOfTheDay: 'false',
+      lng: 'ru',
+      birthPlace: 'Верона, Италия',
+      timelineData: [
+        {
+          date: '1768',
+          text:
+            'Джузеппе де Сакко был приглашён в Варшаву, где занимался художественным катафалком (сastrum doloris) королевы Марии Лещинской и проектом перестройки костёла святого Яна.',
+        },
+        {
+          date: '1771',
+          text:
+            'Стал архитектором Скарбовой комиссии ВКЛ',
+        },
+        {
+          date: '1771-1780',
+          text:
+            'Джузеппе де Сакко выполнил ряд заказов местных магнатов, спроектировав несколько известных дворцово-парковых ансамблей и городских резиденций',
+        },
+      ],
+      work: [
+        {
+          title: 'Дворец в Цёнжени',
+          width: 53.898,
+          length: 27.56,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Ci%C4%85%C5%BCe%C5%84_-_pa%C5%82ac_biskup%C3%B3w_pozna%C5%84skich_03.jpg/225px-Ci%C4%85%C5%BCe%C5%84_-_pa%C5%82ac_biskup%C3%B3w_pozna%C5%84skich_03.jpg',
+        },
+        {
+          title:
+            'Дворец Хрептовичей в Щорсах',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/%C5%A0%C4%8Dorsy%2C_Chraptovi%C4%8D._%D0%A8%D1%87%D0%BE%D1%80%D1%81%D1%8B%2C_%D0%A5%D1%80%D0%B0%D0%BF%D1%82%D0%BE%D0%B2%D1%96%D1%87_%281894%29.jpg/225px-%C5%A0%C4%8Dorsy%2C_Chraptovi%C4%8D._%D0%A8%D1%87%D0%BE%D1%80%D1%81%D1%8B%2C_%D0%A5%D1%80%D0%B0%D0%BF%D1%82%D0%BE%D0%B2%D1%96%D1%87_%281894%29.jpg',
+        },
+        {
+          title:
+            'Театр Тизенгауза',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/%D0%A2%D0%B5%D0%B0%D1%82%D1%80_%D0%BA%D1%83%D0%BA%D0%BE%D0%BB_%D0%B2_%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE.JPG/225px-%D0%A2%D0%B5%D0%B0%D1%82%D1%80_%D0%BA%D1%83%D0%BA%D0%BE%D0%BB_%D0%B2_%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE.JPG',
+        },  
+        {
+          title:
+            'Ансамбль площади Тизенгауза',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/%D0%90%D0%BD%D1%81%D0%B0%D0%BC%D0%B1%D0%BB%D1%8C_%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8_%D0%A2%D0%B8%D0%B7%D0%B5%D0%BD%D0%B3%D0%B0%D1%83%D0%B7%D0%B0.JPG/225px-%D0%90%D0%BD%D1%81%D0%B0%D0%BC%D0%B1%D0%BB%D1%8C_%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8_%D0%A2%D0%B8%D0%B7%D0%B5%D0%BD%D0%B3%D0%B0%D1%83%D0%B7%D0%B0.JPG',
+        },
+        {
+          title:
+            'Святский дворец',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/%C5%9Aviacak._%D0%A1%D1%8C%D0%B2%D1%8F%D1%86%D0%B0%D0%BA_%282006%29.jpg/225px-%C5%9Aviacak._%D0%A1%D1%8C%D0%B2%D1%8F%D1%86%D0%B0%D0%BA_%282006%29.jpg',
+        },
+      ],
+    },
+  ],
+}
