937eaf1db80f7e5a6327c8addfd5db91aeeb85db
pavel <leskovecpavel7@gmail.com>
2019-07-30 20:33:35 +0300

    feat: add list ow works component

--- culture-portal/src/components/ListOfWorksComponent/ListOfWorksComponent.js
+++ culture-portal/src/components/ListOfWorksComponent/ListOfWorksComponent.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import'./ListOfWorksComponent.css';
+import React from "react";
+import "./ListOfWorksComponent.css";
 
-const ListOfWorksComponent = (props) => {
-  
+const ListOfWorksComponent = props => {
   return (
     <div className="container">
-      ListOfWorksComponent
+      <h3>Значимые работы архитектора</h3>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Наименование объекта</th>
+            <th>Дата создания</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.works.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.title}</td>
+                <td>{item.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
 export default ListOfWorksComponent;
