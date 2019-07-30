3ec115d89da1a7bd70ec584f7f0c83ec212637c9
unknown <tarasikuss@gmail.com>
2019-07-30 23:22:28 +0300

    fix: add name field in authors json file

--- culture-portal/src/authors.js
+++ culture-portal/src/authors.js
@@ -2,33 +2,45 @@ exports.frontmatter = {
   authors : [
     {
       id: '1',
+      name: 'Taras Nemau',
       imgPath: 'images/tarasikuss.jpg',
-      name: 'tarasikuss',
+      nickname: 'tarasikuss',
       gitLink: 'https://github.com/tarasikuss',
     },
     {
       id: '2',
+      name: 'Anna Chernoivan',
       imgPath: 'images/nura.jpg',
-      name: 'Nura-BY-13',
+      nickname: 'Nura-BY-13',
       gitLink: 'https://github.com/Nura-BY-13',
     },
     {
       id: '3',
+      name: 'Pavel Leskovets',
       imgPath: 'images/pavel.jpg',
-      name: 'pavel-leskavets',
+      nickname: 'pavel-leskavets',
       gitLink: 'https://github.com/pavel-leskavets',
     },
     {
       id: '4',
+      name: 'Dmitry'
       imgPath: 'images/dmtrby.jpg',
-      name: 'dmtrby',
+      nickname: 'dmtrby',
       gitLink: 'https://github.com/dmtrby',
     },
     {
       id: '5',
+      name: 'Vlad Tataritsky',
       imgPath: 'images/kektor.jpg',
-      name: 'Kektor44',
+      nickname: 'Kektor44',
       gitLink: 'https://github.com/Kektor44',
     },
+    {
+      id: '6',
+      name: 'Yanis Machkin', 
+      imgPath: 'images/yanis.jpg',
+      nickname: 'yanyan155',
+      gitLink: 'https://github.com/yanyan155',
+    },    
   ],
 }
