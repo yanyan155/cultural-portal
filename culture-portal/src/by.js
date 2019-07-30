ede2dc0f4027db6da407a0e932f8b23a3d51fb4f
unknown <tarasikuss@gmail.com>
2019-07-30 21:18:53 +0300

    feat: add by json file

---
+++ culture-portal/src/by.js
@@ -0,0 +1,233 @@
+exports.frontmatter = {
+  authors : [
+    {
+      id: '1',
+      path: '/Ananich',
+      name: 'Юрый Сцяпанавіч Ананіч',
+      date: '23 лістапада 1955 - 28 лютага 2015',
+      img: 'ananich.jpg',
+      videoId: 'LiSW2jgILeQ',
+      vita: 'Беларускі архітэктар. Член Беларускага саюза архітэктараў.',
+      authorOfTheDay: 'true',
+      lng: 'by',
+      birthPlace: 'Мінск',
+      timelineData: [
+        {
+          date: '1978',
+          text:
+            'Скончыў архітэктурны факультэт Беларускага дзяржаўнага політэхнічнага інстытута (цяпер БНТУ) па спецыяльнасці «горадабудаўніцтва».',
+        },
+        {
+          date: '1979-2009',
+          text:
+            'Пасля завяршэння вучобы на працягу трыццаці гадоў працаваў у РУП «Інстытут Белдзяржпраект».',
+        },
+        {
+          date: '2009';,
+          text:
+            'Удзельнічаў у праектаванні трох вышынных будынкаў, якія расійская кампанія «Ітэра» планавала пабудаваць у дадатак да жылога хмарачосу «Ветразь» у рамках шматфункцыянальнага комплексу «Бізнес-цэнтр па вул. М.Танка ».',
+        },
+        {
+          date: '2015',
+          text:
+            'Трагічна загінуў на чыгуначнай станцыі Пціч Мінскага раёна',
+        },
+      ],
+      work: [
+        {
+          title: 'Жылы комплекс пасольства Расійскай федэрацыі ў Беларусі',
+          width: 53.898,
+          length: 27.56,
+          date: '2007',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Russian_Embassy_1.jpg/330px-Russian_Embassy_1.jpg',
+        },
+        {
+          title:
+            'Галаўны офіс БСБ Банк па вуліцы Заслаўскай у Мінску',
+          width: 53.898,
+          length: 27.551,
+          date: '2016',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/BelSwissBank_Headquarters.jpg/300px-BelSwissBank_Headquarters.jpg',
+        },
+        {
+          title: 'Будынак філіяла № 511 АСБ «Беларусбанк»',
+          date: '2012',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Belarusbank_511.jpg/300px-Belarusbank_511.jpg',
+        },
+        {
+          title: 'Жылы комплекс пасольства Расійскай федэрацыі',
+          width: 53.898331,
+          length: 27.560555,
+          date: '2007',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Russian_Embassy_2.jpg/300px-Russian_Embassy_2.jpg',
+        },
+        {
+          title: 'Рэканструкцыя будынка Нацыянальнага банка Беларусі',
+          date: '2002',
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/NBRB.JPG/269px-NBRB.JPG',
+        },
+      ],
+    },
+    {
+      id: '2',
+      path: '/Levin',
+      name: 'Леанід Мендзелевіч Левін',
+      date: '25 ліпеня 1936 - 1 сакавіка 2014',
+      img: 'levin.jpg',
+      videoId: 'qoWJgqu47gA',
+      vita: 'Заслужаны архітэктар Рэспублікі Беларусь.',
+      authorOfTheDay: 'false',
+      lng: 'by',
+      birthPlace: 'Мінск',
+      timelineData: [
+        {
+          date: '1954-1960',
+          text:
+            'Вучоба ў Беларускім політэхнічным інстытуце.',
+        },
+        {
+          date: '1960',
+          text:
+            'Праца ў Інстытуце "Мінскпраект" (ад архітэктара да кіраўніка Майстэрні па забудове цэнтра Мінска).',
+        },
+        {
+          date: '1960-1967';,
+          text:
+            'Удзел у стварэнні парка ім. 50-годдзя Савецкай улады.',
+        },
+        {
+          date: '1968',
+          text:
+            'Удзел у стварэнні выставачнага павільёна ВДНГ БССР у Мінску.',
+        },
+        {
+          date: '1970',
+          text:
+            'У складзе аўтарскага калектыву быў удастоены Ленінскай прэміі за мемарыяльны комплекс 1968-1969 гг. «Хатынь».',
+        },
+      ],
+      work: [
+        {
+          title: 'Мемарыяльны ансамбль "Хатынь"',
+          width: 53.898,
+          length: 27.56,
+          date: '1969',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Khatyn_Memorial%2C_Belarus.jpg/401px-Khatyn_Memorial%2C_Belarus.jpg',
+        },
+        {
+          title:
+            'Помнік Янку Купалу',
+          width: 53.898,
+          length: 27.551,
+          date: '1972',
+          img:
+            'https://upload.wikimedia.org/wikipedia/ru/thumb/f/ff/Yanka-kupala-minsk.jpg/345px-Yanka-kupala-minsk.jpg',
+        },
+        {
+          title:
+            'Помнік Якубу Коласу',
+          width: 53.898,
+          length: 27.551,
+          date: '1972',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Minsk_Thinker_-_panoramio.jpg/401px-The_Minsk_Thinker_-_panoramio.jpg',
+        },
+        {
+          title: 'Станцыя метро «Плошча Леніна»',
+          width: 53.898331,
+          length: 27.560555,
+          date: '1984',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Lenpl_03.jpg/375px-Lenpl_03.jpg',
+        },
+        {
+          title: '«Траецкае прадмесце»',
+          width: 53.898331,
+          length: 27.560555,
+          date: '1980—1986',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Traeckaje_suburb-3.jpg/375px-Traeckaje_suburb-3.jpg',
+        },
+      ],
+    },
+    {
+      id: '3',
+      path: '/Dzuzeppe',
+      name: 'Джузэпэ дэ Сака',
+      date: '1735 — 1798',
+      img: 'dzuzeppe.jpg',
+      videoId: 'TkaLjjNj8Ls',
+      vita: 'Адзін з найбуйнейшых архітэктараў развітога барока і класіцызму, якія працавалі на тэрыторыі сучаснай Беларусі',
+      authorOfTheDay: 'false',
+      lng: 'by',
+      birthPlace: 'Верона, Італія',
+      timelineData: [
+        {
+          date: '1768',
+          text:
+            'Джузэпэ дэ Сака быў запрошаны ў Варшаву, дзе займаўся мастацкім катафалкам (сastrum doloris) каралевы Марыі Ляшчынскі і праектам перабудовы касцёла святога Яна.',
+        },
+        {
+          date: '1771',
+          text:
+            'Cтал архітэктарам Скарбовая камісіі ВКЛ',
+        },
+        {
+          date: '1771-1780';,
+          text:
+            'Джузэпэ дэ Сака выканаў шэраг заказаў мясцовых магнатаў, спраектаваўшы некалькі вядомых палацава-паркавых ансамбляў і гарадскіх рэзідэнцый',
+        },
+      ],
+      work: [
+        {
+          title: 'Палац у Цёнжени',
+          width: 53.898,
+          length: 27.56,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Ci%C4%85%C5%BCe%C5%84_-_pa%C5%82ac_biskup%C3%B3w_pozna%C5%84skich_03.jpg/225px-Ci%C4%85%C5%BCe%C5%84_-_pa%C5%82ac_biskup%C3%B3w_pozna%C5%84skich_03.jpg',
+        },
+        {
+          title:
+            'Палац Храптовічаў у Шчорсах',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/%C5%A0%C4%8Dorsy%2C_Chraptovi%C4%8D._%D0%A8%D1%87%D0%BE%D1%80%D1%81%D1%8B%2C_%D0%A5%D1%80%D0%B0%D0%BF%D1%82%D0%BE%D0%B2%D1%96%D1%87_%281894%29.jpg/225px-%C5%A0%C4%8Dorsy%2C_Chraptovi%C4%8D._%D0%A8%D1%87%D0%BE%D1%80%D1%81%D1%8B%2C_%D0%A5%D1%80%D0%B0%D0%BF%D1%82%D0%BE%D0%B2%D1%96%D1%87_%281894%29.jpg',
+        },
+        {
+          title:
+            'тэатр Тызенгаўза',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/%D0%A2%D0%B5%D0%B0%D1%82%D1%80_%D0%BA%D1%83%D0%BA%D0%BE%D0%BB_%D0%B2_%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE.JPG/225px-%D0%A2%D0%B5%D0%B0%D1%82%D1%80_%D0%BA%D1%83%D0%BA%D0%BE%D0%BB_%D0%B2_%D0%93%D1%80%D0%BE%D0%B4%D0%BD%D0%BE.JPG',
+        },
+        {
+          title:
+            'Ансамбль плошчы Тызенгаўза',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/%D0%90%D0%BD%D1%81%D0%B0%D0%BC%D0%B1%D0%BB%D1%8C_%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8_%D0%A2%D0%B8%D0%B7%D0%B5%D0%BD%D0%B3%D0%B0%D1%83%D0%B7%D0%B0.JPG/225px-%D0%90%D0%BD%D1%81%D0%B0%D0%BC%D0%B1%D0%BB%D1%8C_%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D0%B8_%D0%A2%D0%B8%D0%B7%D0%B5%D0%BD%D0%B3%D0%B0%D1%83%D0%B7%D0%B0.JPG',
+        },
+        {
+          title:
+            'Свяцкі палац',
+          width: 53.898,
+          length: 27.551,
+          date: 'XVIII век',
+          img:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/%C5%9Aviacak._%D0%A1%D1%8C%D0%B2%D1%8F%D1%86%D0%B0%D0%BA_%282006%29.jpg/225px-%C5%9Aviacak._%D0%A1%D1%8C%D0%B2%D1%8F%D1%86%D0%B0%D0%BA_%282006%29.jpg',
+        },
+      ],
+    },
+  ],
+}
