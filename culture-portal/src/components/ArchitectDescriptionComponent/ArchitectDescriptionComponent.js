1771e802ae9bf4ea24ae2f87fe620f24b8c00ae0
Vlad Tataritsky <tataritskii@bk.ru>
2019-07-31 16:19:07 +0300

    feat: add styles for ArchitectDescriptionComponent

--- culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
+++ culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import'./ArchitectDescriptionComponent.css';
+import authorOfTheDay from "../../assets/images/Yury_Ananich.jpg";
 
 const ArchitectDescriptionComponent = (props) => {
-
+  const path = '../../assets/' + props.player.img;
+  console.log(path);
   return (
     <div className="container">
-     {/* <p><img src="assets/Yury_Ananich.jpg" alt="Художник дня"></p>*/}
-      <div className="container">
+      <p><img src={ authorOfTheDay} /></p>
         <h1>{props.player.name}</h1>
         <h4>{props.player.date}</h4>
         <p>{props.player.vita}</p>
-      </div>
     </div>
   )
 }
