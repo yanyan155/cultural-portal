76ebb75f4802ff12197ce0440c0303a9f9ffe23e
Vlad Tataritsky <tataritskii@bk.ru>
2019-07-31 16:47:59 +0300

    fix: delete unless code on component and fix size of img

--- culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
+++ culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import'./ArchitectDescriptionComponent.css';
-import authorOfTheDay from "../../assets/images/Yury_Ananich.jpg";
 
 const ArchitectDescriptionComponent = (props) => {
-  const path = '../../assets/' + props.player.img;
-  console.log(path);
   return (
     <div className="container">
-      <p><img src={ authorOfTheDay} /></p>
+      <p><img src={props.player.img} /></p>
         <h1>{props.player.name}</h1>
         <h4>{props.player.date}</h4>
         <p>{props.player.vita}</p>
