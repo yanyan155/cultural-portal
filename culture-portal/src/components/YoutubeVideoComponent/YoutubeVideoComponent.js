65d33784e75d63d7711b56853223b914146aab5a
pavel-leskavets <43208543+pavel-leskavets@users.noreply.github.com>
2019-08-02 13:25:29 +0300

    Update YoutubeVideoComponent.js

--- culture-portal/src/components/YoutubeVideoComponent/YoutubeVideoComponent.js
+++ culture-portal/src/components/YoutubeVideoComponent/YoutubeVideoComponent.js
@@ -2,7 +2,59 @@ import React from "react";
 import "./YoutubeVideoComponent.css";
 
 const YoutubeVideoComponent = props => {
-  return <div className="container">YoutubeVideoComponent</div>;
+  const path = `https://www.youtube.com/embed/${props.videoId}`;
+  return (
+    <div className="container youtube-block" id="youtube-video">
+      <h3>Youtube</h3>
+      <div className="video">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Смотреть видео
+        </button>
+      </div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                {props.name}
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true" className="close-button">
+                  <i className="fa fa-close" aria-hidden="true" />
+                </span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <iframe
+                width="600"
+                height="400"
+                title="this is a unique title"
+                src={path}
+              />
+            </div>
+            <div className="modal-footer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default YoutubeVideoComponent;
