a7cd042e9086c6d9d0be945dec413fb8ea73fd01
pavel-leskavets <43208543+pavel-leskavets@users.noreply.github.com>
2019-08-02 13:29:19 +0300

    Update index.js

--- culture-portal/src/pages/ArchitectorPage/index.js
+++ culture-portal/src/pages/ArchitectorPage/index.js
@@ -1,32 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import TimeLineComponent from "../../components/TimeLineComponent/index";
-import ListOfWorksComponent from "../../components/ListOfWorksComponent/index";
-import ArchitectDescriptionComponent from "../../components/ArchitectDescriptionComponent/index";
+import React, { Component } from "react";
+import i18next from "i18next";
 
+import TimeLineComponent from "../../components/TimeLineComponent/index";
+import ArchitectPageNavigationComponent from "../../components/ArchitectPageNavigationComponent/index";
+import ListOfWorksComponent from "../../components/ListOfWorksComponent/";
+import YoutubeVideoComponent from "../../components/YoutubeVideoComponent";
 import ArchitectsAPI from "../../ArchitectsAPI";
 
-const Arhitect = props => {
-  const player = ArchitectsAPI.get(parseInt(props.match.params.number, 10));
-  if (!player) {
-    // сюда заимпортить страницу 404 вместо строчки ниже.
-    return <div>404. Такого архитектора нет</div>;
+class Architect extends Component {
+  constructor(props) {
+    super();
+    this.author = ArchitectsAPI.get(props.match.params.name);
   }
-  return (
-    // Компонент навигации по странице архитекторов
-    // Компонент Описания Архитектора
-    // Компонент TimeLine
-    // Компонент Список работ
-    // Компонент Видео с ютуба
-    // Компонент Карта
-    // Компонент Галерея
-    // Верстка примерная - убрать этот комментарий когда прикрутим наш сайт.
-    <div>
-      <ArchitectDescriptionComponent player={player} />
-      <TimeLineComponent timeline={player.timelineData} />
-      <ListOfWorksComponent works={player.work} />
-    </div>
-  );
-};
 
-export default Arhitect;
+  componentWillMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    if (!this.author) {
+      // сюда заимпортить страницу 404 вместо строчки ниже.
+      return <div>404. Такого архитектора нет</div>;
+    } else
+      return (
+        // Компонент навигации по странице архитекторов
+        // Компонент Описания Архитектора
+        // Компонент TimeLine
+        // Компонент Список работ
+        // Компонент Видео с ютуба
+        // Компонент Карта
+        // Компонент Галерея
+        // Верстка примерная - убрать этот комментарий когда прикрутим наш сайт.
+        <div>
+          <h1>{i18next.t(`${this.author.path}:${this.author.name}`)}</h1>
+          <TimeLineComponent author={this.author} />
+          <ListOfWorksComponent author={this.author} />
+          <ArchitectPageNavigationComponent />
+          <YoutubeVideoComponent
+            videoId={this.author.videoId}
+            name={this.author.name}
+          />
+        </div>
+      );
+  }
+}
+
+export default Architect;
