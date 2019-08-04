6a79e6624fdc8a7209c9d88d1f34d46f35330164
yanyan155 <jm@tut.by>
2019-08-04 19:38:47 +0300

    feat: add title to geowidget

--- culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
+++ culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import i18next from "i18next";
 import'./WorksOfArchotectOnMapComponent.css';
-// width="640" height="480"
+
 const WorksOfArchotectOnMapComponent = (props) => {
   let path = `https://www.google.com/maps/d/embed?${props.author.geovidgetLink}&hl=en`
   return (
-    <div className="container text-center mb-4" id="map">
-      <iframe title="geowidget"  src={path}></iframe>
+    <div className="container text-center mb-4 pt-4 geowidget-wrap" id="map">
+    	<h3 className="mb-3">{i18next.t('Map-title')}</h3>
+    	<iframe title="geowidget"  src={path}></iframe>
     </div>
   )
 }
