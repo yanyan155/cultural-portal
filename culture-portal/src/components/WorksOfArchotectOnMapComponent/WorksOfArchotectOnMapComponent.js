55e4526808b842dfcc0da426db26d7f47c88cae3
pavel-leskavets <43208543+pavel-leskavets@users.noreply.github.com>
2019-08-04 16:00:46 +0300

    Update WorksOfArchotectOnMapComponent.js

--- culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
+++ culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
@@ -1,23 +1,13 @@
-import React from "react";
-import "./WorksOfArchotectOnMapComponent.css";
-
-const WorksOfArchotectOnMapComponent = props => {
-  // dd later to geovidgetLink to json
-  let geovidgetLink = "mid=1azzyT0kI_WJG5h08u9p0Kt-Nv_h_FO8s"; // delete string later
-  /*
-	geovidgetLink: 'mid=1azzyT0kI_WJG5h08u9p0Kt-Nv_h_FO8s', // Иосиф Григорьевич Лангбард
-	geovidgetLink: 'mid=1h1uJwA9rkRhJToIj-Wna3cup2YWiBSDE', // Паоло Антонио Доменико Фонтана
-	geovidgetLink: 'mid=1XdWwCcjN5n_7C0kFCxcq2G2r2VYV6fFO', // Виктор Иванович Карако
-	geovidgetLink: 'mid=10ZwynmgbM79zAAagBDpS4fxav_vpOQfO', // Джузеппе де Сакко
-	geovidgetLink: 'mid=1DMvXxw4BPZYozlqBwvqmgzWLMr6HpzH5', // Леонид Менделевич Левин
-	geovidgetLink: 'mid=1vhu3Tx8WxYjPhjjqoz1_mbK6wuDt-yPn', // ананич
-  */
-  let path = `https://www.google.com/maps/d/embed?${geovidgetLink}&hl=en`;
+import React from 'react';
+import'./WorksOfArchotectOnMapComponent.css';
+// width="640" height="480"
+const WorksOfArchotectOnMapComponent = (props) => {
+  let path = `https://www.google.com/maps/d/embed?${props.author.geovidgetLink}&hl=en`
   return (
-    <div className="container text-center mb-4" id="map">
-      <iframe title="geovidget" src={path} width="640" height="480" />
+    <div className="container text-center mb-4" id="map>
+      <iframe title="geowidget"  src={path}></iframe>
     </div>
-  );
-};
+  )
+}
 
 export default WorksOfArchotectOnMapComponent;
