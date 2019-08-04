9ea62035e5b3e7415d2b476a0821c1bdd3c8c08e
dmtrby <dmitry.mahliui@gmail.com>
2019-08-04 19:27:19 +0300

    fix: two h1 on a autors page

--- culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
+++ culture-portal/src/components/ArchitectDescriptionComponent/ArchitectDescriptionComponent.js
@@ -12,7 +12,7 @@ const ArchitectDescriptionComponent = props => {
           className='description-img'
         />
       </p>
-      <h1>{i18next.t(`${props.author.path}:${props.author.name}`)}</h1>
+      <h2>{i18next.t(`${props.author.path}:${props.author.name}`)}</h2>
       <h4>{i18next.t(`${props.author.path}:${props.author.date}`)}</h4>
       <p>{i18next.t(`${props.author.path}:${props.author.vita}`)}</p>
     </div>
