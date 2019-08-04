5e5d3636411a285cb9befcdf7c2fbd1612773e49
dmtrby <dmitry.mahliui@gmail.com>
2019-08-04 19:34:05 +0300

    feat: translate for map component

--- culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
+++ culture-portal/src/components/WorksOfArchotectOnMapComponent/WorksOfArchotectOnMapComponent.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import'./WorksOfArchotectOnMapComponent.css';
+
+import './WorksOfArchotectOnMapComponent.css';
+import i18next from "i18next";
 // width="640" height="480"
 const WorksOfArchotectOnMapComponent = (props) => {
   let path = `https://www.google.com/maps/d/embed?${props.author.geovidgetLink}&hl=en`
   return (
     <div className="container text-center mb-4" id="map">
-      <iframe title="geowidget"  src={path}></iframe>
+      <h3>{i18next.t('Map-of-works')}</h3>
+      <iframe title="geowidget" src={path}></iframe>
     </div>
   )
 }
